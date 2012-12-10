c49911f620eff7b8acfebdbfa67fb5615174279c
Brian Samek <brian.samek@gmail.com>
2012-12-09 21:06:29 -0500

    Began sketching out item.Item.parse_html()

--- item.py
+++ item.py
@@ -1,7 +1,42 @@
 """This module provides the class Item, representing a bibliographic item."""
 
+from bs4 import BeautifulSoup
+
 class Item:
     """A bibliographic item."""
 
+    def __init__(self, page):
+        self.parse_html(page)
+
     def get_bibdata(self):
+        """Pretty print the bib data"""
         return {}
+
+    def parse_html(self, page):
+        self.bibdata = []
+
+        """Parses HTML page of a bib record.
+
+        Returns a 2-dimensional list."""
+
+        # Build the BeautifulSoup tree
+        soup = BeautifulSoup(page)
+
+        # Get tables
+        tables = soup('table')
+        full = tables[2]
+        top, middle, bottom = tables[3], tables[4], tables[5]
+
+        # Parse the headers 
+        headers = full('th')
+        for header in headers:
+            split = header.text.split(':')
+            if len(split) > 1:
+                self.bibdata.append([split[0], split[1].strip()])
+            else:
+                self.bibdata.append(['bibnumber', split[0]])
+
+        print self.bibdata
+
+
+
--- test_item.py
+++ test_item.py
@@ -1,19 +1,19 @@
 import unittest
 import catalog, item, settings
 
-class TestCatalog(unittest.TestCase):
+class TestItem(unittest.TestCase):
 
     def setUp(self):
         self.page = open("bibrecord_example.html", "r").read()
-        self.item = item.Item()
+        self.item = item.Item(self.page)
 
-    def test_returns_dictionary(self):
+    def test_parse_html(self):
         bibdata = self.item.get_bibdata()
-        self.assertIsInstance(bibdata, dict)
-
-    def test_bibnumber_in_dictionary(self):
-        bibdata = self.item.get_bibdata()
-        self.assertEqual(bibdata["bibnumber"], "B2915957x")
-
+        self.assertEqual(bibdata['bibnumber'], ['B2915957x'])
+        self.assertEqual(bibdata['Language'], ['eng'])
+        self.assertEqual(bibdata['Note'], ['Includes index.'])
+        self.assertEqual(bibdata['Standard No.'], 
+            ['9780672330926 (hbk.)', '067233092X (hbk.)'])
+        
 if __name__ == '__main__':
     unittest.main()
